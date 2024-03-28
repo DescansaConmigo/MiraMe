435cf6df471d0c0db761a605d58aac00734585e9
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-03-28 00:21:21 -0700

    Added Basic Files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/client/public'))
+
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+
+
+app.listen(port);
